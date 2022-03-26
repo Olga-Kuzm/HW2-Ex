463e4b7cd40d203edd370626be6f1a4e2bd4f3a7
Olga-Kuzm <legions1987@gmail.com>
2022-03-26 11:15:44 +0300

    add HW2

--- specs/click-hide-button.e2e.js
+++ specs/click-hide-button.e2e.js
@@ -1,14 +1,26 @@
-// REMOVE THE BELOW CODE BEFORE START THE EXERCISE
-describe('Check app', function () {
-    it('click on hide button', async function () {
+describe('Check app', function(){
+    it('should login', async function(){
         await browser.url('https://viktor-silakov.github.io/course-sut');
         await $('#login').setValue('walker@jw.com');
         await $('#password').setValue('password');
         await $('button').click();
-        await browser.pause(3000);
-        await browser.execute('alert(arguments[0] + arguments[1])', 'Hello', ' World!');
-        await browser.pause(3000);
+        await $('#spinner').waitForDisplayed({timeout : 15000, reverse : true});
+
     });
-});
+    it('should delete sticky element', async function(){
+        const elem = await $$('header/*[class*=sticky]')[0];
+        await browser.pause(1000);
+        await browser.execute((el)=> {el.remove()}, elem);           
+        await browser.pause(1000);       
+        
+    });
+    it('should click on hidden button & accept alert', async function(){
+        const button = await $$('button[onclick*=alert')[0];        
+        await browser.execute("arguments[0].click()", button);
+        await browser.pause(1000);               
+        await browser.acceptAlert();
+        await browser.pause(2000)
+    })
+})
 
 
--- specs/login-layout.e2e.js
+++ specs/login-layout.e2e.js
@@ -1,16 +1,41 @@
-// REMOVE THE BELOW CODE BEFORE START THE EXERCISE
-describe('Check app', function () {
-    it('should login', async function () {
+
+describe('Explicit wait & layout check', function(){
+    it('should log in', async function(){
         await browser.url('https://viktor-silakov.github.io/course-sut');
         await $('#login').setValue('walker@jw.com');
         await $('#password').setValue('password');
-        await $('button').click();
-        await browser.pause(15000);
+        await $('button').click();        
+        await $('#spinner').waitForDisplayed({timeout : 15000, reverse : true});
         const title = await browser.getTitle();
         if (title !== 'Report portal') {
             throw new Error('You don`t login into system!!!')
-        }
+        }   
+
     });
-});
+
+    it('should login with correct login', async function(){
+        const user = await $('#user-label');
+        const userName = await user.getText();
+        if(userName !== "John Walker"){
+            throw new Error (`User is not correct`)
+        }
+
+    });
+
+    it('should not be red', async function(){
+        const menu = await $$('#first-nav-block>*');       
+        for (const item of menu) {                        
+            item.moveTo(); 
+            await browser.pause(1000);           
+            const color = await $(item).getCSSProperty('background-color');
+            
+            if(color.value === 'rgba(255,0,0,1)'){
+                const text = await item.getText()
+                throw new Error (`Menu item ${text} has wrong color`)
+            };            
+        }          
+    }) 
+})
+    
 
 
